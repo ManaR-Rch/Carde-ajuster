6efdf141e8ecd55afcf0c7589ee9bf62a545c00b
ManaR-Rch <manarmarchou6@gmail.com>
2024-12-17 20:12:16 +0100

    add addimage function

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function addText() {
   text.className = 'text-item'; 
   card.appendChild(text);
 }
+
+function addImage() {
+  var card = document.getElementById('card');
+  var url = prompt('Entrez l\'URL de l\'image :');
+  if (url) { 
+    var img = document.createElement('img');
+    img.src = url;
+    img.style.width = '100%';
+    card.appendChild(img); 
+  }
+}
